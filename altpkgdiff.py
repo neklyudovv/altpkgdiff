10821d78b017c8994a37c6c78038fadb47c61d1b
Igor Nekludov <igor.nekludovv@gmail.com>
2025-07-29 05:31:27 +0300

    remove main, make branches configurable

--- altpkgdiff.py
+++ altpkgdiff.py
@@ -18,37 +18,27 @@ def compare_versions(v1, r1, v2, r2):
     return vercmp(r1, r2)
 
 
-def main():
-    sisyphus = fetch("sisyphus")
-    p11 = fetch("p11")
-    print("packages in sisyphus: " + str(len(sisyphus)))
-    print("packages in p11: " + str(len(p11)))
-
-    arch_set = set(pkg["arch"] for pkg in sisyphus + p11)
+def diff_branches_json(first_branch, second_branch):
+    arch_set = set(pkg["arch"] for pkg in first_branch + second_branch)
     result = {}
 
     for arch in arch_set:
         # "pkg name": {pkg attr}
-        s_map = {p["name"]: p for p in sisyphus if p["arch"] == arch}
-        p_map = {p["name"]: p for p in p11 if p["arch"] == arch}
+        s_map = {p["name"]: p for p in first_branch if p["arch"] == arch}
+        p_map = {p["name"]: p for p in second_branch if p["arch"] == arch}
 
-        only_in_sisyphus = sorted(set(s_map) - set(p_map))
-        only_in_p11 = sorted(set(p_map) - set(s_map))
+        only_in_first_branch = sorted(set(s_map) - set(p_map))
+        only_in_second_branch = sorted(set(p_map) - set(s_map))
 
         newer = []
         for name in set(s_map) & set(p_map):  # both contains same pkg
             if compare_versions(s_map[name]["version"], s_map[name]["release"],
                                 p_map[name]["version"], p_map[name]["release"]) > 0:
-                newer.append(name)  # but if sisyphus ver is newer - append
+                newer.append(name)  # but if first_branch ver is newer - append
 
         result[arch] = {  # generating json = arch name: {versions}
-            "only_in_sisyphus": only_in_sisyphus,
-            "only_in_p11": only_in_p11,
-            "newer_in_sisyphus": sorted(newer)
+            "only_in_branch1": only_in_first_branch,
+            "only_in_branch2": only_in_second_branch,
+            "newer_in_branch1": sorted(newer)
         }
-
-    print(json.dumps(result))
-
-
-if __name__ == "__main__":
-    main()
+    return json.dumps(result, indent=2)
